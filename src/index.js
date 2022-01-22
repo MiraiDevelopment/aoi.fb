2c02d05eb03ad2b64e7be9c8f83ddada45de0ce9
Krul <39830381+KrulDev@users.noreply.github.com>
2022-01-22 19:51:35 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,30 @@
 const { initializeApp } = require("firebase/app");
 const version = require("../package.json").version
 const docs = require("./methods/docs");
+const axios = require("axios");
+
+async function checkVersion() {
+
+  try {
+    
+    const res = await axios.get("http://node.us1.quizapi.site:25002/package");
+
+      if (version !== res.data.version) {
+
+        console.warn(
+          "\x1b[31maoi.fb warning: \u001b[33mv" +
+          res.data.version +
+          " is available to install.\u001b[0m",
+        )
+
+      }
+      
+  } catch {
+
+  }
+
+}
+  
 class AoiFB {
 
   constructor() {
@@ -23,6 +47,7 @@ class AoiFB {
       
         const app = initializeApp(object);
         console.log('[ Aoi.fb ] - Firebase initialized!')
+        checkVersion()
         return {
           version: version,
           ping: ping,
