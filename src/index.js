4edb193ff2db549a80b7c0cb44bf553605a8be23
KrulDev <silvaluisfernando321@gmail.com>
2022-01-23 14:10:44 -0300

    Added new methods

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const { initializeApp } = require("firebase/app");
+const firebase = require("firebase");
 const { version } = require("../package.json");
-const { checkVersion } = requrie("./API.js");
+const { checkVersion } = require("./API.js");
 
 // Internal Variables
 //let ping = require("./methods/ping");
@@ -17,7 +17,64 @@ class AoiFB {
 
         this.create = function create(object) {
 
-            
+            try {
+
+                const app = firebase.initializeApp(object);
+                const db = firebase.database()
+
+                console.log('[ Aoi.fb ] - Firebase initialized!')
+                checkVersion()
+                return {
+                    table: class table {
+                        constructor(x) {
+                            this.table = x
+                        }
+
+                        set(key, value) {
+                            let result = db.ref(this.table + "/" + key).set(value)
+                            return typeof result !== undefined;
+                        }
+
+                        async get(key) {
+                            let result = await db.ref(this.table + "/" + key).once("value")
+                                result = result.val()
+
+                            return result
+                                ? {
+                                    key: key,
+                                    value: result,
+                                }
+                                : undefined;
+                        }
+
+                        delete() {
+                            console.log("d")
+                        }
+
+                        async all() {
+
+                            let result = await db.ref(this.table + "/").once("value")
+                                result = result.val()
+                                result = Object.entries(result || {})
+
+                            let a = []
+
+                            for (let i = 0; i < result.length; i++) {
+
+                                const doc = result[i]
+
+                                const x = { key: doc[0], value: doc[1] };
+
+                                a.push(x);
+
+                            }
+                            
+                            return a
+                        }
+                    }
+                };
+
+            } catch (e) { throw new Error(e) }
 
         }
 
