eb24d53b66d526dfc783905a1bb0f7ddccd3aa46
SrWhale <contato.pauloideal@gmail.com>
2022-01-24 11:41:00 -0300

    Reformulated method 'all'

--- src/methods/all.js
+++ src/methods/all.js
@@ -1,50 +1,17 @@
-async function all(model, { filter } = {}) {
+const fb = require('firebase');
+
+const db = fb.database();
+
+async function all(model, { filter } = { filter: (data) => data }) {
+    const dbData = await db.ref(model).once('value').then(res => res.val());
     
-    let fb = require("firebase")
-    let db = fb.database()
+    const result = Object.entries(dbData || {});
     
-    if(typeof filter === "function") {
-      
-      let result = await db.ref(model+"/").once("value")
-          result = result.val()
-          result = Object.entries(result || {})
+    return result.map(([key, value]) => {
+        const filterData = { key, data: { key, value } };
+        
+        return filter(filterData) ? filterData : false;
+    }).filter(e => e);
+};
 
-      let a = []
-
-      for(let i = 0; i < result.length; i++) {
-
-        const doc = result[i]
-
-        const x = { key: doc[0], data: { key: doc[0], value: doc[1] } };
-
-        if (filter(x)) a.push(x);
-
-      }
-
-      return a
-      
-    } else {
-    
-      let result = await db.ref(model+"/").once("value")
-          result = result.val()
-          result = Object.entries(result || {})
-
-      let a = []
-
-      for(let i = 0; i < result.length; i++) {
-
-        const doc = result[i]
-
-        const x = { key: doc[0], data: { key: doc[0], value: doc[1] } };
-
-        a.push(x);
-
-      }
-
-      return a
-      
-    }
-  
-  }
-  
-  module.exports = all
+module.exports = all;
