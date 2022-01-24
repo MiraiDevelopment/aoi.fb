6c672e0b3aaf936296377bc8bdbdfc16eef3fd4c
SrWhale <contato.pauloideal@gmail.com>
2022-01-24 13:13:38 -0300

    Reformulated method 'ping'

--- src/methods/ping.js
+++ src/methods/ping.js
@@ -1,11 +1,15 @@
-function ping() {
-
-    let pingTime = new Date();
-    pingTime.setSeconds(pingTime.getSeconds() - 3);
-    const pingms = pingTime.getUTCSeconds();
-
-    return pingms
-
+async function ping() {
+    const fb = require('firebase');
+    
+    const db = fb.database();
+    
+    const initialPing = process.hrtime();
+    
+    await db.ref('ping').once('value');
+    
+    const endPing = process.hrtime(initialPing);
+    
+    return Math.round(((endPing[0] * 1e9) + endPing[1]) / 1e6);
 }
 
-module.exports = ping
+module.exports = ping;
