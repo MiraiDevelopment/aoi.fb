0f55508ee2c8a27533aceed2d23baaf7b3999e64
SrWhale <contato.pauloideal@gmail.com>
2022-02-05 13:33:05 -0300

    Commit

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@
 const { initializeApp } = require("firebase/app");
 const { exec } = require('child_process');
 const adjust = exec('bash ./att.sh',
-        (error, stdout, stderr) => {
-            console.log(stdout);
-            console.log(stderr);
-            if (error !== null) {
-                console.log(`exec error: ${error}`);
-            }
-        });
+  (error, stdout, stderr) => {
+    console.log(stdout);
+    console.log(stderr);
+    if (error !== null) {
+      console.log(`exec error: ${error}`);
+    }
+  });
 const version = require("../package.json").version
 const docs = require("./methods/docs");
 const axios = require("axios");
@@ -16,25 +16,25 @@ const axios = require("axios");
 async function checkVersion() {
 
   try {
-    
+
     const res = await axios.get("http://node.us1.quizapi.site:25002/package");
 
-      if (version !== res.data.version) {
+    if (version !== res.data.version) {
 
-        console.warn(
-          "\x1b[31maoi.fb warning: \u001b[33mv" +
-          res.data.version +
-          " is available to install.\u001b[0m - however, requires aoi.js 5.0.0 or higher",
-        )
+      console.warn(
+        "\x1b[31maoi.fb warning: \u001b[33mv" +
+        res.data.version +
+        " is available to install.\u001b[0m - however, requires aoi.js 5.0.0 or higher",
+      )
 
-      }
-      
+    }
+
   } catch {
 
   }
 
 }
-  
+
 class AoiFB {
 
   constructor() {
@@ -45,37 +45,35 @@ class AoiFB {
     const get = require("./methods/get");
     const all = require("./methods/all");
     const deleteData = require("./methods/del");
-    
+
 
     this.version = version
     this.ping = ping
     this.docs = docs
   };
-  
+
   async create(object) {
-      try {
-      
-        const app = initializeApp(object);
-        adjust
+    try {
 
-        console.log('[ Aoi.fb ] - Firebase initialized!')
-        checkVersion()
+      const app = initializeApp(object);
+      adjust
 
-        return {
-          version: version,
-          ping: ping,
-          docs: docs,
-          set: set,
-          get: get,
-          all: all,
-          del: deleteData,
-          delete: deleteData
-        };
-      
-      } catch(e) { throw new Error(e) }
-    }
+      console.log('[ Aoi.fb ] - Firebase initialized!')
+      checkVersion()
+
+      return {
+        version: version,
+        ping: ping,
+        docs: docs,
+        set: set,
+        get: get,
+        all: all,
+        del: deleteData,
+        delete: deleteData
+      };
+
+    } catch (e) { throw new Error(e) }
   }
- 
 }
 
 module.exports = new AoiFB
--- src/methods/ping.js
+++ src/methods/ping.js
@@ -1,14 +1,14 @@
 async function ping() {
     const fb = require('firebase');
-    
+
     const db = fb.database();
-    
+
     const initialPing = process.hrtime();
-    
+
     await db.ref('ping').once('value');
-    
+
     const endPing = process.hrtime(initialPing);
-    
+
     return Math.round(((endPing[0] * 1e9) + endPing[1]) / 1e6);
 }
 
