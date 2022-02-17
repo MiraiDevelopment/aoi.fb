747cb276daeee25083a5cf09082095a43a71a8be
Guilherme <85150663+Belzinhu@users.noreply.github.com>
2022-02-17 01:33:04 -0300

    Update all.js

--- src/methods/all.js
+++ src/methods/all.js
@@ -1,17 +1,17 @@
 async function all(model, { filter } = { filter: (data) => data }) {
-
-    let fb = require('firebase');
-    let db = fb.database();
-
+    const database = require('firebase').database();
     const dbData = await db.ref(model).once('value').then(res => res.val());
-    
+
     const result = Object.entries(dbData || {});
     
-    return result.map(([key, value]) => {
-        const filterData = { key, data: { key, value } };
-        
-        return filter(filterData) ? filterData : false;
-    }).filter(e => e);
+    return  result.reduce((acc, [key, value]) => {
+        if(value) {
+          const filterData = { key, data: { key, value } };
+          if(filter(filterData)) acc.push(filterData);
+        };
+        /*Dessa forma, só há 1 loop da outra forma há 2 loop, isso é uma má prática e má otimização do código espero ter ajudado.*/
+        return acc;
+    }, [])
 };
 
 module.exports = all;
