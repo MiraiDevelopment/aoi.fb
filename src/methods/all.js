3efa004c51c573e9bacabc78a5e39062d521d4b4
guilhermedsribeiro <guiguiribeiro.2020@gmail.com>
2022-02-18 22:05:38 -0300

    Update Method All

--- src/methods/all.js
+++ src/methods/all.js
@@ -1,5 +1,5 @@
 async function all(model, { filter } = { filter: (data) => data }) {
-    const database = require('firebase').database();
+    const db = require('firebase').database();
     const dbData = await db.ref(model).once('value').then(res => res.val());
 
     const result = Object.entries(dbData || {});
@@ -9,7 +9,6 @@ async function all(model, { filter } = { filter: (data) => data }) {
           const filterData = { key, data: { key, value } };
           if(filter(filterData)) acc.push(filterData);
         };
-        /*Dessa forma, só há 1 loop da outra forma há 2 loop, isso é uma má prática e má otimização do código espero ter ajudado.*/
         return acc;
     }, [])
 };
